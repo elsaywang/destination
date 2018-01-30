384f4a98c38c88d17d3865880848dc66bd045fe6
Yan <mangakjd@gmail.com>
2018-01-30 17:52:46 -0500

    more cleanup

--- src/actions.js
+++ src/actions.js
@@ -3,8 +3,10 @@ import { createAction } from 'redux-actions';
 export const CALL_SEARCH = 'CALL_SEARCH';
 export const CALL_SEARCH_FULFILLED = 'CALL_SEARCH_FULFILLED';
 export const CALL_SEARCH_REJECTED = 'CALL_SEARCH_REJECTED';
-export const callSearch = createAction(CALL_SEARCH, path => {
-    return new Promise(function() {
-        return false;
-    });
-});
+export const callSearch = createAction(
+    CALL_SEARCH,
+    path =>
+        new Promise(() => ({
+            search: path,
+        })),
+);
