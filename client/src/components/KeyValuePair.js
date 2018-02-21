8dbd8b79383abbca0b62c4404d428e86654e98b8
Yan <yanl@adobe.com>
2018-02-21 15:43:42 -0500

    bind operator change method in constructor instead of render method

--- client/src/components/KeyValuePair.js
+++ client/src/components/KeyValuePair.js
@@ -1,36 +1,51 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Select from '@react/react-spectrum/Select';
 import Search from '@react/react-spectrum/Search';
 import Label from './Label';
 
-function KeyValuePair({ onKeyChange, onOperatorChange, onValueChange, pair }) {
-    const operatorOptions = [
-        { label: '=', value: '=' },
-        { label: '>', value: '>' },
-        { label: '<', value: '<' },
-    ];
-    const { id, key, value, operator } = pair;
+const operatorOptions = [
+    { label: '=', value: '=' },
+    { label: '>', value: '>' },
+    { label: '<', value: '<' },
+];
 
-    return (
-        <Fragment>
-            <Label value="Key">
-                <Search
-                    id={id}
-                    value={key}
-                    placeholder="Type a key or key name"
-                    onChange={onKeyChange}
+class KeyValuePair extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onSelectOperatorChange = this.props.onOperatorChange.bind(this, this.props.pair.id);
+    }
+
+    render() {
+        const { onKeyChange, onValueChange, pair } = this.props;
+        const { id, key, value, operator } = pair;
+
+        return (
+            <Fragment>
+                <Label value="Key">
+                    <Search
+                        id={id}
+                        value={key}
+                        placeholder="Type a key or key name"
+                        onChange={onKeyChange}
+                    />
+                </Label>
+                <Select
+                    value={operator}
+                    onChange={this.onSelectOperatorChange}
+                    options={operatorOptions}
                 />
-            </Label>
-            <Select
-                value={operator}
-                onChange={onOperatorChange.bind(this, id)}
-                options={operatorOptions}
-            />
-            <Label value="Value">
-                <Search id={id} value={value} placeholder="Type a value" onChange={onValueChange} />
-            </Label>
-        </Fragment>
-    );
+                <Label value="Value">
+                    <Search
+                        id={id}
+                        value={value}
+                        placeholder="Type a value"
+                        onChange={onValueChange}
+                    />
+                </Label>
+            </Fragment>
+        );
+    }
 }
 
 export default KeyValuePair;
