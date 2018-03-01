65230696b7be7e709780fcb8250dc846e1187773
Dylan Kario <kario@adobe.com>
2018-03-01 13:12:10 -0800

    AAM-34808: [WIP] Add table component with basic columns
    Set up a dummy table using the `TableView` component, which
    requires a `TableViewDataSource` component that defines the
    table's columns and overrides a few base table methods.

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,8 +1,119 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { TableView, TableViewDataSource } from '@react/react-spectrum/TableView';
 
-class Table extends Component {
-    render() {
-        return <div>{this.props.data}</div>;
+const columns = {
+    keyValuePair: {
+        title: 'Key-Value Pair',
+        key: 'keyValuePair',
+        minWidth: 150,
+    },
+    keyName: {
+        title: 'Key Name',
+        key: 'keyName',
+        width: 150,
+    },
+    valueName: {
+        title: 'Value Name',
+        key: 'valueName',
+        width: 150,
+    },
+    signalType: {
+        title: 'Signal Type',
+        key: 'signalType',
+        width: 140,
+    },
+    signalSource: {
+        title: 'Signal Source',
+        key: 'signalSource',
+        width: 180,
+    },
+    totalCounts: {
+        title: 'Total Counts',
+        key: 'totalCounts',
+        width: 100,
+    },
+    totalEventFires: {
+        title: 'Total Event Fires',
+        key: 'totalEventFires',
+        width: 100,
+    },
+    percentageChange: {
+        title: 'Percentage Change',
+        key: 'percentageChange',
+        width: 120,
+    },
+    includedInTraits: {
+        title: 'Included In Traits',
+        key: 'includedInTraits',
+        width: 120,
+    },
+};
+
+// TODO: Actionable Log Files and General Online Data don't show Signal Source
+const basicColumns = [
+    columns.keyValuePair,
+    columns.signalType,
+    columns.signalSource,
+    columns.totalCounts,
+    columns.percentageChange,
+    columns.includedInTraits,
+];
+
+const analyticsColumns = [
+    columns.keyValuePair,
+    columns.keyName,
+    columns.valueName,
+    columns.totalEventFires,
+    columns.percentageChange,
+    columns.includedInTraits,
+];
+
+const renderCell = (column, data) => <span>{data}</span>;
+
+// Dynamically set the height of the table's container to show up to a certain number of rows.
+const getTableHeight = (items, maxRows = 10) => {
+    const headHeight = 40;
+    const rowHeight = 48;
+    const bodyHeight = Math.min(items.length, maxRows) * rowHeight;
+
+    return `${headHeight + bodyHeight}px`;
+};
+
+const Table = ({ items }) => {
+    const dataSource = new DataSource({ items });
+
+    return (
+        <div style={{ height: getTableHeight(items) }}>
+            <TableView dataSource={dataSource} renderCell={renderCell} />
+        </div>
+    );
+};
+
+class DataSource extends TableViewDataSource {
+    constructor({ items }) {
+        super();
+
+        this.items = items;
+    }
+    // Override
+    getColumns() {
+        // TODO: Find a way to dynamically return a column group based on a signalType prop.
+        // Stuck because the `TableViewDataSource` superclass calls `getColumns()` before props are available.
+        return basicColumns;
+    }
+    // Override
+    getNumberOfRows() {
+        return this.items.length;
+    }
+    // Override
+    getCell(column, row) {
+        return this.items[row][column.key];
+    }
+    // Override
+    sort(column, direction) {
+        this.items.sort((a, b) => (a[column.key] < b[column.key] ? -direction : direction));
+
+        this.reloadData();
     }
 }
 
--- client/src/containers/SearchContainer.js
+++ client/src/containers/SearchContainer.js
@@ -5,12 +5,31 @@ import SearchFilters from './SearchFilters';
 import Table from '../components/Table';
 import { callSearch } from '../actions';
 
+const items = [
+    {
+        keyValuePair: 'eVar1=acb382hfj',
+        signalType: 'Actionable Log Files',
+        signalSource: '—',
+        totalCounts: '4,194,138',
+        percentageChange: '+ 24.41%',
+        includedInTraits: '2 Traits',
+    },
+    {
+        keyValuePair: 'eVar4=CRM12345',
+        signalType: 'Onboarded Records',
+        signalSource: 'Data Source ABC DEF GHI JKL MNO',
+        totalCounts: '1,139,451',
+        percentageChange: '- 7.82%',
+        includedInTraits: '3 Traits',
+    },
+];
+
 function SearchContainer(props) {
     return (
         <Fragment>
             <h1 className={styles.header}>{props.location.pathname}</h1>
             <SearchFilters onSearch={props.callSearch} path={props.location.pathname} />
-            <Table data={props.results} />
+            <Table items={items} data={props.results} />
         </Fragment>
     );
 }
