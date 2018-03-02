0fde84fd38cb6269673bff353a468b3c39201a61
Riajur Tuhin <tuhin@tuhin-macos.corp.adobe.com>
2018-03-02 10:55:37 -0500

    AAM:35717- Routing between ui pages

--- client/cypress/integration/dashboard_spec.js
+++ client/cypress/integration/dashboard_spec.js
@@ -1,6 +1,6 @@
 describe('Dashboard Integration Tests', function() {
     it('should navigate user to dashboard after clicking Signals on top nav', function(params) {
-        cy.visit('http://localhost:3000');
+        cy.visit('http://localhost:3000/#/');
 
         cy.get('h1').should('include', 'Dashboard');
         // cy.title().should('include', 'Dashboard')
---
+++ client/cypress/integration/routing_spec.js
@@ -0,0 +1,19 @@
+describe('Integration Tests for routing', function() {
+    it('should check the ability for routing in the UI', function() {
+        // navigate to Signal Home Page
+        cy.visit('http://localhost:3000');
+        // verify - it is signsl home page
+        cy.title().should('contain', 'Signals');
+        // click on surch link
+        cy.contains('Search').click();
+        // verify the search page
+        cy
+            .get('button')
+            .contains('Search')
+            .should('be.exist');
+        // click on Dashboard button
+        cy.contains('Dashboard').click();
+        //veriufy - the dashbord page
+        cy.get('.spectrum-Table').should('be.exist');
+    });
+});
