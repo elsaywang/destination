8b17fb443787e48b5de33220d4432b1abc5695f9
Yan <yanl@adobe.com>
2018-03-04 15:25:06 -0500

    convert SelectList back to TabList

--- client/src/components/SignalSourceFilter.js
+++ client/src/components/SignalSourceFilter.js
@@ -1,22 +1,29 @@
 import React, { Fragment } from 'react';
-import SelectList from '@react/react-spectrum/SelectList';
+import { TabList, Tab } from '@react/react-spectrum/TabList';
 import Heading from '@react/react-spectrum/Heading';
 import styles from './SignalSourceFilter.css';
 import getSignalSourceOptions from '../constants/getSignalSourceOptions';
 
 function SignalSourceFilter({ filter, counts, handleSignalSourceChange }) {
     const signalSourceOptions = getSignalSourceOptions(counts);
+    const renderTabs = () => {
+        return signalSourceOptions.map(option => (
+            <Tab selected={filter === option.value}>{option.label}</Tab>
+        ));
+    };
 
     return (
         <Fragment>
             <Heading size={3} className={styles.heading}>
                 Filter By Signal Source:
             </Heading>
-            <SelectList
+            <TabList
+                className={styles.signalSource}
+                orientation="vertical"
                 onChange={handleSignalSourceChange}
-                options={signalSourceOptions}
-                value={filter}
-            />
+                value={filter}>
+                {renderTabs()}
+            </TabList>
         </Fragment>
     );
 }
