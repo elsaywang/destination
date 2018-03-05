b9e97e7eacfdfddecc3fd3c86c683df239fa537a
Yan <yanl@adobe.com>
2018-03-05 03:23:13 -0500

    AAM-34807: add SearchContainer test

---
+++ client/src/test/SearchContainer.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SearchContainer from '../containers/SearchContainer';
+import SearchFilters from '../containers/SearchFilters';
+import SelectList from '@react/react-spectrum/SelectList';
+import SignalSourceFilter from '../components/SignalSourceFilter';
+import Table from '../components/Table';
+import configureStore from '../configureStore';
+
+describe('<SearchContainer /> component', () => {
+    const initialState = {
+        counts: {
+            all: 72093,
+            adobeAnalytics: 34300,
+            actionableLogFiles: 359,
+            generalOnlineData: 27,
+            onboardedRecords: 37407,
+        },
+        filter: 'all',
+    };
+    const wrapper = shallow(<SearchContainer store={configureStore()} />).dive();
+
+    describe('rendering', () => {
+        it('matches snapshot', () => {
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('renders <SearchFilters /> component', () => {
+            expect(wrapper.find(SearchFilters).exists()).toBe(true);
+        });
+
+        it('renders <SignalSourceFilter /> component', () => {
+            expect(wrapper.find(SignalSourceFilter).exists()).toBe(true);
+        });
+
+        it('renders <Table /> component', () => {
+            expect(wrapper.find(Table).exists()).toBe(true);
+        });
+    });
+
+    describe('state changes based on events', () => {
+        it('.handleSignalSourceChange() changes state.filter', () => {
+            const newFilter = 'adobeAnalytics';
+
+            expect(wrapper.state('filter')).toBe(initialState.filter);
+            wrapper.instance().handleSignalSourceChange(newFilter);
+            expect(wrapper.state('filter')).toBe(newFilter);
+        });
+    });
+});
