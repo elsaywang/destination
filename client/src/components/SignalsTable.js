8e1c63ee0625c7bad56bd7a43ea0694263ed659b
Dylan Kario <kario@adobe.com>
2018-03-08 16:20:15 -0800

    AAM-34808: Each signal type filter can show a unique column group
    Made different variables for each column group since there's a good
    chance each one will be unique.

--- client/src/components/SignalsTable.js
+++ client/src/components/SignalsTable.js
@@ -1,22 +1,42 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Table from './common/Table';
-import { analyticsColumns, basicColumns } from '../constants/columns';
+import {
+    allSignalsColumns,
+    analyticsColumns,
+    advancedAnalyticsColumns,
+    actionableLogFilesColumns,
+    generalOnlineDataColumns,
+    onboardedRecordsColumns,
+} from '../constants/columns';
 
 class SignalsTable extends Component {
-    getColumns(signalType) {
-        return signalType === 'adobeAnalytics' ? analyticsColumns : basicColumns;
+    render() {
+        const { items, signalType } = this.props;
+        const columns = this.getColumns(signalType);
+
+        return <Table items={items} columns={columns} renderCell={this.renderCell} />;
     }
 
     renderCell(column, data) {
         return <span>{data}</span>;
     }
 
-    render() {
-        const { items, signalType } = this.props;
-        const columns = this.getColumns(signalType);
-
-        return <Table items={items} columns={columns} renderCell={this.renderCell} />;
+    getColumns(signalType, isAdvancedSearchEnabled = false) {
+        switch (signalType) {
+            case 'all':
+                return allSignalsColumns;
+            case 'adobeAnalytics':
+                return isAdvancedSearchEnabled ? analyticsColumns : advancedAnalyticsColumns;
+            case 'actionableLogFiles':
+                return actionableLogFilesColumns;
+            case 'generalOnlineData':
+                return generalOnlineDataColumns;
+            case 'onboardedRecords':
+                return onboardedRecordsColumns;
+            default:
+                return allSignalsColumns;
+        }
     }
 }
 
--- client/src/constants/columns.js
+++ client/src/constants/columns.js
@@ -1,66 +1,86 @@
-const columns = {
-    keyValuePair: {
-        title: 'Key-Value Pair',
-        key: 'keyValuePair',
-        minWidth: 150,
-    },
-    keyName: {
-        title: 'Key Name',
-        key: 'keyName',
-        width: 150,
-    },
-    valueName: {
-        title: 'Value Name',
-        key: 'valueName',
-        width: 150,
-    },
-    signalType: {
-        title: 'Signal Type',
-        key: 'signalType',
-        width: 140,
-    },
-    signalSource: {
-        title: 'Signal Source',
-        key: 'signalSource',
-        width: 180,
-    },
-    totalCounts: {
-        title: 'Total Counts',
-        key: 'totalCounts',
-        width: 100,
-    },
-    totalEventFires: {
-        title: 'Total Event Fires',
-        key: 'totalEventFires',
-        width: 100,
-    },
-    percentageChange: {
-        title: 'Percentage Change',
-        key: 'percentageChange',
-        width: 120,
-    },
-    includedInTraits: {
-        title: 'Included In Traits',
-        key: 'includedInTraits',
-        width: 120,
-    },
+/**
+ * Individual columns
+ */
+const keyValuePair = {
+    title: 'Key-Value Pair',
+    key: 'keyValuePair',
+    minWidth: 150,
+};
+const keyName = {
+    title: 'Key Name',
+    key: 'keyName',
+    width: 150,
+};
+const valueName = {
+    title: 'Value Name',
+    key: 'valueName',
+    width: 150,
+};
+const signalType = {
+    title: 'Signal Type',
+    key: 'signalType',
+    width: 140,
+};
+const signalSource = {
+    title: 'Signal Source',
+    key: 'signalSource',
+    width: 180,
+};
+const totalCounts = {
+    title: 'Total Counts',
+    key: 'totalCounts',
+    width: 100,
+};
+const totalEventFires = {
+    title: 'Total Event Fires',
+    key: 'totalEventFires',
+    width: 100,
+};
+const percentageChange = {
+    title: 'Percentage Change',
+    key: 'percentageChange',
+    width: 120,
+};
+const includedInTraits = {
+    title: 'Included In Traits',
+    key: 'includedInTraits',
+    width: 120,
 };
 
-// TODO: Actionable Log Files and General Online Data don't show Signal Source
-export const basicColumns = [
-    columns.keyValuePair,
-    columns.signalType,
-    columns.signalSource,
-    columns.totalCounts,
-    columns.percentageChange,
-    columns.includedInTraits,
+/**
+ * Column groups by signal type
+ */
+export const allSignalsColumns = [
+    keyValuePair,
+    signalType,
+    signalSource,
+    totalCounts,
+    percentageChange,
+    includedInTraits,
 ];
 
-export const analyticsColumns = [
-    columns.keyValuePair,
-    columns.keyName,
-    columns.valueName,
-    columns.totalEventFires,
-    columns.percentageChange,
-    columns.includedInTraits,
+// These are the same for now
+export const analyticsColumns = [...allSignalsColumns];
+
+export const advancedAnalyticsColumns = [
+    keyValuePair,
+    keyName,
+    valueName,
+    totalEventFires,
+    percentageChange,
+    includedInTraits,
 ];
+
+export const actionableLogFilesColumns = [
+    keyValuePair,
+    signalType,
+    totalCounts,
+    percentageChange,
+    includedInTraits,
+];
+
+// These are the same for now
+export const generalOnlineDataColumns = [...actionableLogFilesColumns];
+
+// These are the same for now
+export const onboardedRecordsColumns = [...allSignalsColumns];
