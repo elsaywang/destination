a4447511b99ea0cb0d7871b5fd513d70c67812ff
Dylan Kario <kario@adobe.com>
2018-03-26 12:53:28 -0700

    AAM-34809: Use css module @value export for maxBarWidth variable
    Now we can use it in PercentageChange.js as the scaling factor for
    how wide to make the colored bar.

--- client/src/components/common/PercentageChange/PercentageChange.js
+++ client/src/components/common/PercentageChange/PercentageChange.js
@@ -4,21 +4,18 @@ import { FormattedNumber } from 'react-intl';
 import classNames from 'classnames';
 import styles from './PercentageChange.css';
 
-const maxBarWidth = 2; // Coupled to half of 4rem width of the .barContainer class.
-
 class PercentageChange extends Component {
     /**
      * Get the width of the colored bar that visualizes the percentage change.
      * @param  {number} percentageChange       prop, used for width of colored bar
      * @param  {Number} maxPercentageMagnitude prop, used to normalize bar's width
-     * @param  {maxBarWidth} maxBarWidth       constant, used as scaling factor
      * @return {string}                        width of colored bar in `rem`
      */
     getBarWidth({ percentageChange, maxPercentageMagnitude }) {
         const minWidth = 0.1 * maxPercentageMagnitude;
         const normalizedBarWidth =
             (Math.abs(percentageChange) + minWidth) / (maxPercentageMagnitude + minWidth);
-        const scaledBarWidth = normalizedBarWidth * maxBarWidth;
+        const scaledBarWidth = normalizedBarWidth * styles.maxBarWidth;
         const barWidth = Math.round(scaledBarWidth * 100) / 100;
 
         return `${barWidth}rem`;
