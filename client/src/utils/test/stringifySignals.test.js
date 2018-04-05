7598ebd8e31d565bfcf8ca3c2760e94a896a8720
Dylan Kario <kario@adobe.com>
2018-04-04 18:04:05 -0700

    AAM-36340: [fixup] Extracted stringifySignals tests
    Just removed it from the 'Utils' and 'Stringifying signals and their key-value pairs' describes and put it in another file.

---
+++ client/src/utils/test/stringifySignals.test.js
@@ -0,0 +1,123 @@
+import { stringifySignals } from '../stringifySignals';
+
+describe('stringifySignals', () => {
+    it('should stringify a single key-value pair from a single signal', () => {
+        const signals = [
+            {
+                keyValuePairs: [
+                    {
+                        key: 'eVar1',
+                        value: 'xyz123',
+                    },
+                ],
+            },
+        ];
+
+        expect(stringifySignals(signals)).toEqual('eVar1=="xyz123"');
+    });
+    it('should stringify multiple key-value pairs from a single signal', () => {
+        const signals = [
+            {
+                keyValuePairs: [
+                    {
+                        key: 'eVar1',
+                        value: 'xyz123',
+                    },
+                    {
+                        key: 'abc123',
+                        value: 'cba321',
+                    },
+                ],
+            },
+        ];
+
+        expect(stringifySignals(signals)).toEqual('eVar1=="xyz123" AND abc123=="cba321"');
+    });
+    it('should stringify single key-value pairs from multiple signals', () => {
+        const signals = [
+            {
+                keyValuePairs: [
+                    {
+                        key: 'eVar1',
+                        value: 'xyz123',
+                    },
+                ],
+            },
+            {
+                keyValuePairs: [
+                    {
+                        key: 'def456',
+                        value: 'qrs789',
+                    },
+                ],
+            },
+        ];
+
+        expect(stringifySignals(signals)).toEqual('eVar1=="xyz123" OR def456=="qrs789"');
+    });
+    it('should stringify multiple key-value pairs from multiple signals', () => {
+        const signals = [
+            {
+                keyValuePairs: [
+                    {
+                        key: 'eVar1',
+                        value: 'xyz123',
+                    },
+                    {
+                        key: 'eVar2',
+                        value: 'zyx321',
+                    },
+                ],
+            },
+            {
+                keyValuePairs: [
+                    {
+                        key: 'def456',
+                        value: 'qrs789',
+                    },
+                    {
+                        key: 'ghi000',
+                        value: 'ihg000',
+                    },
+                ],
+            },
+        ];
+
+        expect(stringifySignals(signals)).toEqual(
+            'eVar1=="xyz123" AND eVar2=="zyx321" OR def456=="qrs789" AND ghi000=="ihg000"',
+        );
+    });
+    it('should stringify empty key-value pairs as an empty string', () => {
+        expect(stringifySignals([])).toEqual('');
+    });
+    describe('value formatting', () => {
+        it('should wrap the value with double quotes if it is a string', () => {
+            const signals = [
+                {
+                    keyValuePairs: [
+                        {
+                            key: 'eVar1',
+                            value: 'xyz123',
+                        },
+                    ],
+                },
+            ];
+
+            expect(stringifySignals(signals).includes('"xyz123"')).toBeTruthy();
+        });
+        it('should not wrap the value with double quotes if it is a number', () => {
+            const signals = [
+                {
+                    keyValuePairs: [
+                        {
+                            key: 'eVar1',
+                            value: 123,
+                        },
+                    ],
+                },
+            ];
+
+            expect(stringifySignals(signals).includes('"123"')).toBeFalsy();
+        });
+    });
+});
