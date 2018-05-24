eaf4ce8c83cedef40b94d566e577ae8a260d472b
Dylan Kario <kario@adobe.com>
2018-05-24 12:00:59 -0700

    AAM-37564: [fixup] Fix remaining portal endpoints

--- client/src/actions/reportSuites.js
+++ client/src/actions/reportSuites.js
@@ -4,7 +4,7 @@ export const GET_REPORT_SUITES = 'GET_REPORT_SUITES';
 export const GET_REPORT_SUITES_FULFILLED = 'GET_REPORT_SUITES_FULFILLED';
 export const GET_REPORT_SUITES_REJECTED = 'GET_REPORT_SUITES_REJECTED';
 export const getReportSuites = createAction(GET_REPORT_SUITES, async function getReportSuites() {
-    const result = await fetch('/api/v1/report-suites');
+    const result = await fetch('/portal/api/v1/report-suites');
 
     return result.json();
 });
