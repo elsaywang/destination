654c94ff3e7137fd6dcf69d2a8e8e7c0bb3ee648
Elsa Wang <elwang@adobe.com>
2018-06-06 19:00:41 -0400

    removed unused until on saveSearch

--- client/src/utils/savedSearch.js
+++
@@ -1,4 +0,0 @@
-export const isLimitReached = (savedSearchlist, limit) => Boolean(savedSearchlist.length >= limit);
-
-export const normalizeSavedSearchList = (savedSearchlist, limit) =>
-    isLimitReached(savedSearchlist, limit) ? [...savedSearchlist].slice(0, limit) : savedSearchlist;
