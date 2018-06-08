6e593051e24828515cd4ee9f8ed6b83b703a6a9e
Dylan Kario <kario@adobe.com>
2018-06-08 14:04:06 -0700

    Replace deferred with mockResponse in search_results_spec

--- client/cypress/integration/search_results_spec.js
+++ client/cypress/integration/search_results_spec.js
@@ -1,54 +1,29 @@
-const deferred = require('../utils/deferred');
+const mockResponse = require('../utils/mockResponse');
 const savedSearchResponse = require('../fixtures/savedSearch.json');
 const searchResultsResponse = require('../fixtures/searchResults.json');
 const datasourcesResponse = require('../fixtures/reportSuites.json');
 const emptySearchResultsResponse = require('../fixtures/emptySearchResults.json');
 
-describe.skip('Search Form Results Integration Tests', function() {
+describe('Search Form Results Integration Tests', function() {
+    beforeEach(function() {
+        cy.visit('#/search', {
+            onBeforeLoad(win) {
+                cy
+                    .stub(win, 'fetch')
+                    .withArgs('/portal/api/v1/users/self/annotations/aam-portal')
+                    .as('fetchSavedSearch')
+                    .returns(mockResponse(savedSearchResponse.savedSearch))
+                    .withArgs('/portal/api/v1/signals/list')
+                    .as('fetchSearchResults')
+                    .returns(mockResponse(searchResultsResponse))
+                    .withArgs('/portal/api/v1/report-suites')
+                    .as('fetchReportSuites')
+                    .returns(mockResponse(datasourcesResponse.list));
+            },
+        });
+    });
     describe('when Search button is clicked', function() {
         describe('it has search results', function() {
-            beforeEach(function() {
-                this.fetchSavedSearchDeferred = deferred();
-                this.fetchSearchResultsDeferred = deferred();
-                this.fetchReportSuitesDeferred = deferred();
-
-                cy.visit('#/search', {
-                    onBeforeLoad(win) {
-                        cy.stub(win, 'fetch')
-                            .withArgs('/portal/api/v1/users/self/annotations/aam-portal')
-                            .as('fetchSavedSearch')
-                            .returns(this.fetchSavedSearchDeferred.promise)
-                            .withArgs('/portal/api/v1/signals/list')
-                            .as('fetchSearchResults')
-                            .returns(this.fetchSearchResultsDeferred.promise)
-                            .withArgs('/portal/api/v1/report-suites')
-                            .as('fetchReportSuites')
-                            .returns(this.fetchReportSuitesDeferred.promise);
-                    },
-                });
-
-                this.fetchSavedSearchDeferred.resolve({
-                    json() {
-                        return savedSearchResponse.savedSearch;
-                    },
-                    ok: true,
-                });
-
-                this.fetchSearchResultsDeferred.resolve({
-                    json() {
-                        return searchResultsResponse;
-                    },
-                    ok: true,
-                });
-
-                this.fetchReportSuitesDeferred.resolve({
-                    json() {
-                        return datasourcesResponse.list;
-                    },
-                    ok: true,
-                });
-            });
-
             it('should render results table', function() {
                 cy.get('[data-test="search-button"]').click();
                 cy.get('[data-test="signals-table"]').should('have.length', 1);
@@ -57,44 +32,10 @@ describe.skip('Search Form Results Integration Tests', function() {
 
         describe('it has no search result', function() {
             beforeEach(function() {
-                this.fetchSavedSearchDeferred = deferred();
-                this.fetchSearchResultsDeferred = deferred();
-                this.fetchReportSuitesDeferred = deferred();
-
-                cy.visit('#/search', {
-                    onBeforeLoad(win) {
-                        cy.stub(win, 'fetch')
-                            .withArgs('/portal/api/v1/users/self/annotations/aam-portal')
-                            .as('fetchSavedSearch')
-                            .returns(this.fetchSavedSearchDeferred.promise)
-                            .withArgs('/portal/api/signals/list')
-                            .as('fetchSearchResults')
-                            .returns(this.fetchSearchResultsDeferred.promise)
-                            .withArgs('/portal/api/v1/report-suites')
-                            .as('fetchReportSuites')
-                            .returns(this.fetchReportSuitesDeferred.promise);
-                    },
-                });
-
-                this.fetchSavedSearchDeferred.resolve({
-                    json() {
-                        return savedSearchResponse.savedSearch;
-                    },
-                    ok: true,
-                });
-
-                this.fetchSearchResultsDeferred.resolve({
-                    json() {
-                        return emptySearchResultsResponse;
-                    },
-                    ok: true,
-                });
-
-                this.fetchReportSuitesDeferred.resolve({
-                    json() {
-                        return datasourcesResponse.list;
-                    },
-                    ok: true,
+                cy.get('@fetchSearchResults').then(function($stub) {
+                    $stub
+                        .as('fetchEmptySearchResults')
+                        .returns(mockResponse(emptySearchResultsResponse));
                 });
             });
 
