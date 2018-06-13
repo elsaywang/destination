d4a920bbaa2b149b1eb7f1ef639bedd50cc14a4a
Dylan Kario <kario@adobe.com>
2018-06-13 13:41:12 -0700

    Convert search_form_spec to user cy.server()
    Also refactor the command `getRequestParams` to accept an XHR

--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -25,7 +25,7 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('getRequestParams', requestAlias =>
-    cy.get(requestAlias).then($request => JSON.parse($request.lastCall.args[1].body)),
+    cy.get(requestAlias).then($xhr => $xhr.request.body),
 );
 
 // Until 'fetch' can be properly mocked, remove `fetch` to force tests to executes XHRs.
