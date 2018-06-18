06739c25bcc629fd1c0a92539712cb34dc700f05
Elsa Wang <elwang@adobe.com>
2018-06-18 11:12:19 -0400

    if current clicked saved search is delete, search results should be reset to initial state, or else it would still display the deleted records

--- client/src/containers/SearchContainer.js
+++ client/src/containers/SearchContainer.js
@@ -26,10 +26,9 @@ import EmptySearch from '../components/EmptySearch';
 import styles from './SearchContainer.css';
 
 class SearchContainer extends Component {
-    constructor() {
-        super();
-
-        this.state = {
+    state = this.initialState();
+    initialState() {
+        return {
             name: '',
             keyValuePairs: [
                 {
@@ -249,35 +248,13 @@ class SearchContainer extends Component {
         const newSavedSearch = [...savedSearch.slice(0, index), ...savedSearch.slice(index + 1)];
 
         saveSearch(newSavedSearch);
+        this.setState(this.initialState());
     };
 
     onClearAll = () => {
         this.props.clearSearch();
         this.props.clearSearchFields();
-        this.setState({
-            name: '',
-            searched: false,
-            keyValuePairs: [
-                {
-                    id: 0,
-                    key: '',
-                    operator: '==',
-                    value: '',
-                },
-            ],
-            signalStatus: 'ALL',
-            advanced: false,
-            source: {
-                name: '',
-                dataSourceIds: [],
-                reportSuiteIds: [],
-                sourceType: 'ALL',
-            },
-            viewRecordsFor: '7D',
-            customStartDate: getDefaultCustomStartDate(),
-            customEndDate: getDefaultCustomEndDate(),
-            minEventFires: 1000,
-        });
+        this.setState(this.initialState());
     };
 
     isCustomDateRangeEnabled = () => this.state.viewRecordsFor === 'custom';
