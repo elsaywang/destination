c353cf33c8af16a9e71fc6e71d87b0729c3e2c64
Dylan Kario <kario@adobe.com>
2018-06-19 09:28:34 -0700

    AAM-37970: Prettier fixes

--- client/cypress/integration/saved_search_spec.js
+++ client/cypress/integration/saved_search_spec.js
@@ -7,21 +7,18 @@ const searchResultsResponse = require('../fixtures/searchResults.json');
 describe('Saved Search Integration Test', function() {
     beforeEach(function() {
         cy.server();
-        cy
-            .route(
-                '/portal/api/v1/users/self/annotations/aam-portal',
-                savedSearchResponse.savedSearch,
-            )
-            .as('fetchSavedSearch');
-        cy
-            .route('POST', '/portal/api/v1/signals/list', searchResultsResponse)
-            .as('fetchSearchResults');
+        cy.route(
+            '/portal/api/v1/users/self/annotations/aam-portal',
+            savedSearchResponse.savedSearch,
+        ).as('fetchSavedSearch');
+        cy.route('POST', '/portal/api/v1/signals/list', searchResultsResponse).as(
+            'fetchSearchResults',
+        );
 
         cy.visit('#/search');
     });
     it("requests user's saved searches", function() {
-        cy
-            .wait('@fetchSavedSearch')
+        cy.wait('@fetchSavedSearch')
             .its('status')
             .should('eq', 200);
     });
@@ -49,9 +46,10 @@ describe('Saved Search Integration Test', function() {
         });
 
         it('should pre-fill Key-Value Pair fields', function() {
-            cy
-                .get('[data-test="key-value-pair"]')
-                .should('have.length', savedSearchResponse.savedSearch[0].keyValuePairs.length);
+            cy.get('[data-test="key-value-pair"]').should(
+                'have.length',
+                savedSearchResponse.savedSearch[0].keyValuePairs.length,
+            );
 
             cy.get('[data-test="key-search-field"]:first').should(function($value) {
                 expect($value.val()).to.eq(savedSearchResponse.savedSearch[0].keyValuePairs[0].key);
@@ -117,9 +115,9 @@ describe('Saved Search Integration Test', function() {
                         cy.get('[data-test="save-this-search-name-field"]').type(searchName);
                     });
 
-                    cy
-                        .get('.spectrum-Dialog-footer .spectrum-Button.spectrum-Button--secondary')
-                        .click();
+                    cy.get(
+                        '.spectrum-Dialog-footer .spectrum-Button.spectrum-Button--secondary',
+                    ).click();
                     cy.get('[data-test="save-this-search-button"]').click();
                     cy.get('[data-test="save-this-search-name-field"]').should('be.empty');
                 });
@@ -132,13 +130,11 @@ describe('Saved Search Integration Test', function() {
                         newSavedSearchResponse,
                     );
 
-                    cy
-                        .route(
-                            'PUT',
-                            '/portal/api/v1/users/self/annotations/aam-portal',
-                            mockResponseAfterCreate,
-                        )
-                        .as('createSavedSearch');
+                    cy.route(
+                        'PUT',
+                        '/portal/api/v1/users/self/annotations/aam-portal',
+                        mockResponseAfterCreate,
+                    ).as('createSavedSearch');
                 });
 
                 it("should save the user's current search", function() {
@@ -148,19 +144,17 @@ describe('Saved Search Integration Test', function() {
                         cy.get('[data-test="save-this-search-name-field"]').type(searchName);
                     });
 
-                    cy
-                        .get('.spectrum-Dialog-footer .spectrum-Button.spectrum-Button--primary')
-                        .click();
+                    cy.get(
+                        '.spectrum-Dialog-footer .spectrum-Button.spectrum-Button--primary',
+                    ).click();
 
                     cy.wait('@createSavedSearch');
 
-                    cy
-                        .get('[data-test="saved-search-tag"]')
+                    cy.get('[data-test="saved-search-tag"]')
                         .contains(searchName)
                         .should('exist');
 
-                    cy
-                        .get('[data-test="saved-search-tag"]')
+                    cy.get('[data-test="saved-search-tag"]')
                         .contains(searchName)
                         .trigger('mouseover');
                     cy.get('[data-test="saved-search-overlay-trigger"]').contains(searchName);
@@ -172,31 +166,29 @@ describe('Saved Search Integration Test', function() {
             it("should remove that saved search from user's saved search", function() {
                 const mockResponseAfterDelete = savedSearchResponse.savedSearch.slice(1);
 
-                cy
-                    .route(
-                        'PUT',
-                        '/portal/api/v1/users/self/annotations/aam-portal',
-                        mockResponseAfterDelete,
-                    )
-                    .as('deleteSavedSearch');
+                cy.route(
+                    'PUT',
+                    '/portal/api/v1/users/self/annotations/aam-portal',
+                    mockResponseAfterDelete,
+                ).as('deleteSavedSearch');
 
                 cy.get('#isCurrentSearch ~ div > [data-test="saved-search-delete-button"]').click();
 
-                cy
-                    .get(
-                        '[data-test="saved-search-delete-modal"] ~ .spectrum-Dialog-footer .spectrum-Button--warning',
-                    )
-                    .click();
+                cy.get(
+                    '[data-test="saved-search-delete-modal"] ~ .spectrum-Dialog-footer .spectrum-Button--warning',
+                ).click();
 
                 cy.wait('@deleteSavedSearch');
 
-                cy
-                    .get('[data-test="saved-search-tag"]')
-                    .should('have.length', savedSearchResponse.savedSearch.length - 1);
+                cy.get('[data-test="saved-search-tag"]').should(
+                    'have.length',
+                    savedSearchResponse.savedSearch.length - 1,
+                );
 
-                cy
-                    .get('[data-test="saved-search-tag"]:first')
-                    .should('not.have.text', savedSearchResponse.savedSearch[0].name);
+                cy.get('[data-test="saved-search-tag"]:first').should(
+                    'not.have.text',
+                    savedSearchResponse.savedSearch[0].name,
+                );
             });
         });
     });
@@ -218,28 +210,24 @@ describe('Saved Search Integration Test', function() {
             cy.get('@topUnusedSignalsTag').should('be.visible');
             cy.get('@newUnusedSignalsTag').should('be.visible');
 
-            cy
-                .get('[data-test="saved-search-tag"]:first')
-                .should('have.attr', 'data-saved-search-preset', 'top-unused-signals');
-            cy
-                .get(
-                    '[data-test="saved-search-tag-list"] > span:nth-child(2) > [data-test="saved-search-tag"]',
-                )
-                .should('have.attr', 'data-saved-search-preset', 'new-unused-signals');
-            cy
-                .get(
-                    '[data-test="saved-search-tag-list"] > span:nth-child(3) > [data-test="saved-search-tag"]',
-                )
-                .should('have.text', savedSearchResponse.savedSearch[0].name);
+            cy.get('[data-test="saved-search-tag"]:first').should(
+                'have.attr',
+                'data-saved-search-preset',
+                'top-unused-signals',
+            );
+            cy.get(
+                '[data-test="saved-search-tag-list"] > span:nth-child(2) > [data-test="saved-search-tag"]',
+            ).should('have.attr', 'data-saved-search-preset', 'new-unused-signals');
+            cy.get(
+                '[data-test="saved-search-tag-list"] > span:nth-child(3) > [data-test="saved-search-tag"]',
+            ).should('have.text', savedSearchResponse.savedSearch[0].name);
         });
 
         it('should render when no user-defined saved searches exist', () => {
-            cy
-                .route(
-                    '/portal/api/v1/users/self/annotations/aam-portal',
-                    emptySavedSearchResponse.savedSearch,
-                )
-                .as('fetchEmptySavedSearch');
+            cy.route(
+                '/portal/api/v1/users/self/annotations/aam-portal',
+                emptySavedSearchResponse.savedSearch,
+            ).as('fetchEmptySavedSearch');
             cy.reload();
 
             cy.wait('@fetchEmptySavedSearch');
@@ -256,42 +244,50 @@ describe('Saved Search Integration Test', function() {
 
         it('"Top Unused Signals" popover should contain all correct search fields', () => {
             cy.get('@topUnusedSignalsTag').trigger('mouseover');
-            cy
-                .get('[data-test="saved-search-popover-name"]')
-                .should('have.text', 'Top Unused Signals');
+            cy.get('[data-test="saved-search-popover-name"]').should(
+                'have.text',
+                'Top Unused Signals',
+            );
             cy.get('[data-test="saved-search-popover-search-query"]').should('have.text', '""==""');
-            cy
-                .get('[data-test="saved-search-popover-signal-category"]')
-                .should('have.text', 'Real-Time');
+            cy.get('[data-test="saved-search-popover-signal-category"]').should(
+                'have.text',
+                'Real-Time',
+            );
             cy.get('[data-test="saved-search-popover-signal-type"]').should('not.be.visible');
             cy.get('[data-test="saved-search-popover-signal-source"]').should('not.be.visible');
             cy.get('[data-test="saved-search-popover-filter-new"]').should('not.be.visible');
-            cy
-                .get('[data-test="saved-search-popover-view-records-for"]')
-                .should('have.text', 'Last 7 Days');
-            cy
-                .get('[data-test="saved-search-popover-sort-by"]')
-                .should('have.text', 'Percentage Change');
+            cy.get('[data-test="saved-search-popover-view-records-for"]').should(
+                'have.text',
+                'Last 7 Days',
+            );
+            cy.get('[data-test="saved-search-popover-sort-by"]').should(
+                'have.text',
+                'Percentage Change',
+            );
         });
 
         it('"New Unused Signals" popover should contain all correct search fields', () => {
             cy.get('@newUnusedSignalsTag').trigger('mouseover');
-            cy
-                .get('[data-test="saved-search-popover-name"]')
-                .should('have.text', 'New Unused Signals');
+            cy.get('[data-test="saved-search-popover-name"]').should(
+                'have.text',
+                'New Unused Signals',
+            );
             cy.get('[data-test="saved-search-popover-search-query"]').should('have.text', '""==""');
-            cy
-                .get('[data-test="saved-search-popover-signal-category"]')
-                .should('have.text', 'Real-Time');
+            cy.get('[data-test="saved-search-popover-signal-category"]').should(
+                'have.text',
+                'Real-Time',
+            );
             cy.get('[data-test="saved-search-popover-signal-type"]').should('not.be.visible');
             cy.get('[data-test="saved-search-popover-signal-source"]').should('not.be.visible');
             cy.get('[data-test="saved-search-popover-filter-new"]').should('have.text', 'Yes');
-            cy
-                .get('[data-test="saved-search-popover-view-records-for"]')
-                .should('have.text', 'Last 7 Days');
-            cy
-                .get('[data-test="saved-search-popover-sort-by"]')
-                .should('have.text', 'Percentage Change');
+            cy.get('[data-test="saved-search-popover-view-records-for"]').should(
+                'have.text',
+                'Last 7 Days',
+            );
+            cy.get('[data-test="saved-search-popover-sort-by"]').should(
+                'have.text',
+                'Percentage Change',
+            );
         });
 
         it('clicking "Top Unused Signals" should populate form correctly and call correct search', () => {
@@ -304,8 +300,7 @@ describe('Saved Search Integration Test', function() {
             cy.get('[data-test="view-records"]').should('have.value', '7D');
             cy.get('[data-test="min-counts"]').should('have.value', '1000');
 
-            cy
-                .get('@fetchSearchResults')
+            cy.get('@fetchSearchResults')
                 .its('status')
                 .should('eq', 200);
         });
@@ -320,8 +315,7 @@ describe('Saved Search Integration Test', function() {
             cy.get('[data-test="view-records"]').should('have.value', '7D');
             cy.get('[data-test="min-counts"]').should('have.value', '1000');
 
-            cy
-                .get('@fetchSearchResults')
+            cy.get('@fetchSearchResults')
                 .its('status')
                 .should('eq', 200);
             cy.getRequestParams('@fetchSearchResults').then(({ filterNewSignals }) => {
@@ -338,13 +332,11 @@ describe('Saved Search Integration Test', function() {
         });
 
         it('should not be included in the PUT call executed after saving a new search', () => {
-            cy
-                .route(
-                    'PUT',
-                    '/portal/api/v1/users/self/annotations/aam-portal',
-                    savedSearchResponse.savedSearch,
-                )
-                .as('createSavedSearch');
+            cy.route(
+                'PUT',
+                '/portal/api/v1/users/self/annotations/aam-portal',
+                savedSearchResponse.savedSearch,
+            ).as('createSavedSearch');
 
             cy.get('[data-test="search-button"]').click();
             cy.get('[data-test="save-this-search-button"]').click();
@@ -353,34 +345,30 @@ describe('Saved Search Integration Test', function() {
 
             cy.getRequestParams('@createSavedSearch').then(savedSearchList => {
                 expect(
-                    savedSearchList.filter(
-                        savedSearch => typeof savedSearch.presetId !== 'undefined',
-                    ).length,
+                    savedSearchList.filter(savedSearch => savedSearch.presetId !== null).length,
                 ).to.eq(0);
             });
         });
 
         it('should be counted toward the limit of max saved searches', () => {
-            cy
-                .route(
-                    '/portal/api/v1/users/self/annotations/aam-portal',
-                    maxSavedSearchResponse.savedSearch,
-                )
-                .as('fetchMaxSavedSearch');
+            cy.route(
+                '/portal/api/v1/users/self/annotations/aam-portal',
+                maxSavedSearchResponse.savedSearch,
+            ).as('fetchMaxSavedSearch');
             cy.reload();
 
             cy.get('[data-test="saved-search-tag"]').should('have.length', 10);
 
             cy.get('[data-test="search-button"]').click();
 
-            cy
-                .get('[data-test="overlay-tooltip"')
+            cy.get('[data-test="overlay-tooltip"')
                 .should('be.visible')
                 .trigger('mouseover');
 
-            cy
-                .get('[data-test="saved-search-limit-message"]')
-                .should('have.text', 'Saved search limit (10) reached');
+            cy.get('[data-test="saved-search-limit-message"]').should(
+                'have.text',
+                'Saved search limit (10) reached',
+            );
         });
     });
 });
