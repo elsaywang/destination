eea2ab813f3c36cf8f68edb6cbcd5b05b0cc68bf
Yan <yanl@adobe.com>
2018-07-18 17:42:06 -0400

    AAM-39070: keyValuePairs and keyName needs to sortBy: key

--- client/src/utils/normalizeSortOptions.js
+++ client/src/utils/normalizeSortOptions.js
@@ -1,7 +1,8 @@
 import { sortColumns } from '../constants/columns';
 
 export const normalizeSortBy = ({ sortBy }) =>
-    sortColumns.map(col => col.key).includes(sortBy) && { sortBy };
+    sortColumns.map(col => col.key).includes(sortBy) &&
+    (sortBy === 'keyValuePairs' || sortBy === 'keyName' ? { sortBy: 'key' } : { sortBy });
 
 // If no `descending` param is passed, then the `/signal/list` API returns
 // results sorted in descending order, regardless of the `sortBy` param.
