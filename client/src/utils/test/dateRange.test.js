150f349774a0f03bca66a66cf551576da15b85fb
Elsa Wang <elwang@adobe.com>
2018-08-06 12:32:04 -0400

    uncommented util test

--- client/src/utils/test/dateRange.test.js
+++ client/src/utils/test/dateRange.test.js
@@ -105,14 +105,14 @@ describe('date range utils', () => {
             expect(actual).toEqual(expected);
         });
     });
-    // describe('getCurrentYear', () => {
-    //     it('should return the current year ', () => {
-    //         const actual = getCurrentYear().valueOf();
-    //         const expected = '2018';
-    //
-    //         expect(actual).toEqual(expected);
-    //     });
-    // });
+    describe('getCurrentYear', () => {
+        it('should return the current year ', () => {
+            const actual = getCurrentYear().valueOf();
+            const expected = '2018';
+
+            expect(actual).toEqual(expected);
+        });
+    });
     describe('getDefaultCustomStartDate', () => {
         it('should return the date of the default # of days ago in the format YYYY-MM-DD', () => {
             const actual = getDefaultCustomStartDate();
