afcb115862b0cb287413cfbaac167ade1bc5c270
nameyer <nameyer@adobe.com>
2018-11-29 11:44:43 -0500

    update constant for default max rows in tables per design spec

--- client/src/constants/rows.js
+++ client/src/constants/rows.js
@@ -2,4 +2,4 @@ export const defaultRowHeight = 48;
 export const maxRowHeight = 72;
 export const defaultHeadHeight = 40;
 export const defaultHeadHeightWithoutSelection = 20;
-export const defaultMaxRows = 10;
+export const defaultMaxRows = 9;
