5871e29455ba9001f5585bec029249256ca710a8
mdwyer <mdwyer@adobe.com>
2019-02-22 14:21:10 -0500

    change people-based category to integrated platforms (#290)

--- packages/destinations/src/constants/destinations.js
+++ packages/destinations/src/constants/destinations.js
@@ -2,7 +2,7 @@
 export const destinationsMap = {
     PUSH: { name: 'URL', category: 'Custom' },
     ADS: { name: 'Cookie', category: 'Custom' },
-    S2S: { name: 'Device-Based', category: 'Integrated Platforms' },
     ANALYTICS: { name: 'Adobe Analytics', category: 'Adobe Experience Cloud' },
-    PEOPLE_BASED: { name: 'People-Based', category: 'Adobe Experience Cloud' },
+    S2S: { name: 'Device-Based', category: 'Integrated Platforms' },
+    PEOPLE_BASED: { name: 'People-Based', category: 'Integrated Platforms' },
 };
